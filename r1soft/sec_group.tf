d0da1ca245811cacae51def83411ae3e198f370a
leventelibal <leventelibal@gmail.com>
2019-08-07 21:18:40 -0400

    fix sec add

--- r1soft/sec_group.tf
+++ r1soft/sec_group.tf
@@ -3,33 +3,31 @@ resource "aws_security_group" "r1soft" {
   description = "Allow inbound traffic"
   vpc_id      = "${var.vpc}"
 
-    ingress {
-   
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
-   
     from_port   = 8443
     to_port     = 8443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
-   
   }
 
-
-     tags = {
-      Name       = "${var.Name}.public"
-      Env        = "${var.Env}"
+  tags = {
+      Name = "${var.Name}.public"
+      Env = "${var.Env}"
       Created_by = "${var.Created_by}"
-      Dept       = "${var.Dept}"
-     }    
+      Dept = "${var.Dept}"
+  }
 }
