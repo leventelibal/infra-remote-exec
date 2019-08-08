23fe60b9cc8613fcd8c2edfd9d8791815c6320c8
leventelibal <leventelibal@gmail.com>
2019-08-07 21:59:09 -0400

    asas

--- template/backend.tf
+++ template/backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         region = "us-east-1"
-        bucket = "remote-exec-levent"
+        bucket = "remote-exec-example"
         key = "example/example.state"
     }
 }
